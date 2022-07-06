9c337c68eb3d58b2b10ca249b735510ea199334b
LichKiller <adonis.villalva@outlook.com>
2022-07-05 20:14:00 -0500

    ssl fixed

--- src/components/common/Slider.jsx
+++ src/components/common/Slider.jsx
@@ -10,7 +10,7 @@ const Slider = () => {
             method: 'GET',
             redirect: 'follow'
         };
-        const data = await fetch("https://api.collectors-empire.com/slider?_format=json", requestOptions);
+        const data = await fetch("http://api.collectors-empire.com/slider?_format=json", requestOptions);
         const items = await data.json();
         setItems(items);
     }
@@ -20,7 +20,7 @@ const Slider = () => {
                 {
                     items.map((item, index) => (
                         <li key = {index} style = {{opacity: 1}}>
-                            <img src = {"https://api.collectors-empire.com/" + item.image} alt = 'slider_img'/>
+                            <img src = {"http://api.collectors-empire.com/" + item.image} alt = 'slider_img'/>
                         </li>
                     ))
                 }           
