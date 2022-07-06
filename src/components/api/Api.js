56ebd073be2be340d052e4f6c2f30dc570575d78
LichKiller <adonis.villalva@outlook.com>
2022-07-05 19:42:26 -0500

    version fixed

--- src/components/api/Api.js
+++ src/components/api/Api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 /*-----URL Bases de las API-----*/
 const Api_Collectos = 'http://collectors.enragestudio.com/api/collectors?_format=json';
 const Base_COllectors = 'http://collectors.enragestudio.com';
-const Base_Collectors = 'http://api.collectors-empire.com/';
+const Base_Collectors = 'https://api.collectors-empire.com/';
 /*-----Funciones para obtener información-----*/
 export function getCollectors(path){
     return axios.get(Base_Collectors + path);
--- src/components/common/Slider.jsx
+++ src/components/common/Slider.jsx
@@ -10,7 +10,7 @@ const Slider = () => {
             method: 'GET',
             redirect: 'follow'
         };
-        const data = await fetch("http://api.collectors-empire.com/slider?_format=json", requestOptions);
+        const data = await fetch("https://api.collectors-empire.com/slider?_format=json", requestOptions);
         const items = await data.json();
         setItems(items);
         console.log(items);
@@ -21,7 +21,7 @@ const Slider = () => {
                 {
                     items.map((item, index) => (
                         <li key = {index} style = {{opacity: 1}}>
-                            <img src = {"http://api.collectors-empire.com/" + item.image} alt = 'slider_img'/>
+                            <img src = {"https://api.collectors-empire.com/" + item.image} alt = 'slider_img'/>
                         </li>
                     ))
                 }           
